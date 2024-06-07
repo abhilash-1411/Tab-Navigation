bc5f68aa7ca122bf6d9add923e51cc16510429ba
abhilash-1411 <Abhilash.Chandravanshi@adixoo.com>
2024-06-07 15:04:44 +0530

    top navigation

--- App.tsx
+++ App.tsx
@@ -2,8 +2,38 @@ import React from 'react';
 import {StyleSheet, Text, View,} from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+// const Tab=createBottomTabNavigator();
+// const App=()=>{
 
-const Tab=createBottomTabNavigator();
+//   return (
+//     <NavigationContainer>
+//       <Tab.Navigator>
+//         <Tab.Screen name='Login' component={Login} />
+//         <Tab.Screen name='SignUp' component={SignUp} />
+
+//       </Tab.Navigator>
+//     </NavigationContainer>
+//   );
+// }
+
+// const Login=()=>{
+//   return(
+//     <View style={{flex:1,alignItems:'center',justifyContent:'center'}}>
+//     <Text style={{fontSize:40}}>Login</Text>
+//   </View>
+//   )
+// }
+// const SignUp=()=>{
+//   return(
+//     <View style={{flex:1,alignItems:'center',justifyContent:'center'}}>
+//       <Text style={{fontSize:40}}>SignUp</Text>
+//     </View>
+//   )
+// }
+
+
+const Tab=createMaterialTopTabNavigator();
 const App=()=>{
 
   return (
@@ -11,6 +41,7 @@ const App=()=>{
       <Tab.Navigator>
         <Tab.Screen name='Login' component={Login} />
         <Tab.Screen name='SignUp' component={SignUp} />
+        <Tab.Screen name='User' component={SignUp} />
 
       </Tab.Navigator>
     </NavigationContainer>
@@ -31,7 +62,6 @@ const SignUp=()=>{
     </View>
   )
 }
-
 
 
 export default App;
